2b005c7f7b720c7b27982f69aa9252988cdcd039
arcvelit <louka.f.s@gmail.com>
2022-10-22 16:33:02 -0400

    Pushing this

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,43 @@
 import React, { useEffect } from "react";
-
-
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-
-
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
+import {  Button } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 
-import Tooltip from '@mui/material/Tooltip';
-
-import { Select, MenuItem, FormControl, Collapse, Button } from "@mui/material";
 
 function Header() {
- 
-  const pages = ['Products', 'Pricing', 'Blog'];
-  const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+  const rows = [
+    { id: 1, col1: "Hello", col2: "World" },
+    { id: 2, col1: "XGrid", col2: "is Awesome" },
+    { id: 3, col1: "Material-UI", col2: "is Amazing" },
+    { id: 4, col1: "Hello", col2: "World" },
+    { id: 5, col1: "XGrid", col2: "is Awesome" },
+    { id: 6, col1: "Material-UI", col2: "is Amazing" }
+  ];
+  
+  const columns = [
+    { field: "id", hide: true },
+    { field: "col1", headerName: "Column 1", width: 150 },
+    { field: "col2", headerName: "Column 2", width: 150 }
+  ];
+  const status_codes = ['CLR', 'DNL', 'RFI', 'RFS', 'WTG'];
+  const status_colors = {
+    'CLR': 'green',
+    'DNL': 'red',
+    'RFI': 'orange',
+    'RFS': 'orange',
+    'WTG': 'yellow',
+}
   return (
     <>
-  
-   <AppBar position="static">
+   <AppBar sx={{ bgcolor: "white" }} position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-        
+          
           <Typography
             variant="h6"
             noWrap
@@ -38,11 +49,11 @@ function Header() {
               fontFamily: 'monospace',
               fontWeight: 700,
               letterSpacing: '.3rem',
-              color: 'inherit',
+              color: 'black',
               textDecoration: 'none',
             }}
           >
-            LOGO
+            &#60;/&#62;BORDERLESS
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
@@ -59,43 +70,26 @@ function Header() {
          
           </Box>
 
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
+    
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
+            {status_codes.map((status) => (
               <Button
-                key={page}
+                key={status}
          
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ fontSize: 12, my: 2, color: 'black', display: 'flex' }}
               >
-                {page}
+                {status + " - 100"}
               </Button>
             ))}
           </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-          <Button variant="outlined">Create  Booking</Button>
-        
-          </Box>
+          
         </Toolbar>
       </Container>
     </AppBar>
+    <div style={{ height: 300, width: "100%" }}>
+      <DataGrid rows={rows} columns={columns} />
+    </div>
     </>
   );
 }
