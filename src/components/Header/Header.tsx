0482f05edaf2da22ace7bec5e219b45d6b0e5ceb
khushbu <khushbu.patel@aircanada.ca>
2022-10-22 14:24:25 -0400

    commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,103 @@
+import React, { useEffect } from "react";
+
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+
+
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+
+import Tooltip from '@mui/material/Tooltip';
+
+import { Select, MenuItem, FormControl, Collapse, Button } from "@mui/material";
+
+function Header() {
+ 
+  const pages = ['Products', 'Pricing', 'Blog'];
+  const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+  return (
+    <>
+  
+   <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+        
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+           
+              color="inherit"
+            >
+          
+            </IconButton>
+         
+          </Box>
+
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+         
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+          <Button variant="outlined">Create  Booking</Button>
+        
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+    </>
+  );
+}
+
+export default Header;
